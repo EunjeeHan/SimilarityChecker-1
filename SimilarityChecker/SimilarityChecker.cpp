6cb928577454863321f2f8d479405f7ba0a7aad4
eunjee <silverhan38@gmail.com>
2023-07-06 11:30:23 +0900

    [refactoring] extract assert method in production code

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -4,21 +4,24 @@ using namespace std;
 class SimilarityChecker
 {
 public:
-	void check(const string& str1, const string& str2)
+	void assertInvalidArgument(const string& str1, const string& str2)
 	{
 		for (char ch : str1)
 		{
-			if (ch < 'A' || ch > 'Z')
-			{
-				throw invalid_argument("Must be alphabet");
-			}
+			if (ch >= 'A' && ch <= 'Z')
+				continue;
+			throw invalid_argument("Must be alphabet");
 		}		
 		for (char ch : str2)
 		{
-			if (ch < 'A' || ch > 'Z')
-			{
-				throw invalid_argument("Must be alphabet");
-			}
-		}		
+			if (ch >= 'A' && ch <= 'Z')
+				continue;
+			throw invalid_argument("Must be alphabet");
+		}
+	}
+
+	void check(const string& str1, const string& str2)
+	{
+		assertInvalidArgument(str1, str2);
 	}
 };
